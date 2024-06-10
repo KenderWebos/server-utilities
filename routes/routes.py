b5b74e7034f7e37b59ae8c2a6975bcede0f6454a
Kevin Campos Venegas <kenderman.8@gmail.com>
2024-06-10 13:53:46 -0400

    update routes.sh option menu

--- routes/routes.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print("Directorio actual:", os.getcwd())
-
-directorio_deseado = "/ruta/del/directorio/deseado"
-os.chdir(directorio_deseado)
-
-print("Nuevo directorio actual:", os.getcwd())
--- routes/routes.sh
+++ routes/routes.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
-# source routes.sh ht
 
-case $1 in
-    here)
-        pwd
-    ;;
-    ht)
-        cd ./programing/github/HeriTech
-    ;;
-    sockets)
-        cd ./programing/sockets/learning-sockets/propuestas/chess-js
-    ;;
-    *)
-        echo "Uso: source routes.sh [nombre]"
-    ;;
-esac
+# Función para mostrar el menú
+mostrar_menu() {
+  echo "Menú principal"
+  echo "1. ir a repositorios GitHub"
+  echo "2. none"
+  echo "3. none"
+  echo "4. Salir"
+}
+
+# Función para obtener la entrada del usuario
+obtener_entrada() {
+  read -p "Ingrese una opción: " opcion
+}
+
+# Función para ejecutar la acción correspondiente
+ejecutar_accion() {
+  case $opcion in
+    1) cd programing/github;;
+    2) echo "Ejecutando none";;
+    3) echo "Ejecutando none";;
+    4) exit 0;;
+    *) echo "Opción inválida";;
+  esac
+}
+
+# Mostrar el menú
+mostrar_menu
+
+# Obtener la entrada del usuario
+obtener_entrada
+
+# Ejecutar la acción correspondiente
+ejecutar_accion
